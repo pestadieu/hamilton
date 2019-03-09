7b60f7c56f50905b1dfc138232f2b22872bc5b77
Adrian <Adrian_Gustavo.Patzi_figueroa@student.reutlingen-university.de>
2019-03-09 03:07:02 +0100

    add maps modules and map component

--- src/my-app/src/app/app.module.ts
+++ src/my-app/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, enableProdMode } from '@angular/core';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
+import { MapComponent } from './map/map.component';
+ 
+enableProdMode();
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({apiKey: 'AIzaSyBBzvSR3OxSELz8vin0qnPwQgYlPHHCIdc'}),
+    FormsModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [GoogleMapsAPIWrapper],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
